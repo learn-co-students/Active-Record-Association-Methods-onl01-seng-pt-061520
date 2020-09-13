fa5115a3dda63b2ef490d2434e1a7f93feb9b304
Cameron Hudson <camnn3@yahoo.com>
2020-09-13 21:36:32 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.collect do |s|
+    self.artists.collect do |t|
+      t.name
   end
 end
